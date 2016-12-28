f7b694d9d42e7f898d496a207e8cf0cd7c4d48be
Piotr Celigowski <piotrcel2@gmail.com>
2016-12-28 18:49:51 +0100

    first

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    
+    console.log('elo');
+    
+    var example = document.getElementById('example');
+    var context = example.getContext('2d');
+    context.fillStyle = "rgb(255,0,0)";
+    context.fillRect(10, 30, 50, 50);
+    
+});
