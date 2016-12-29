6482550099ce6593d23e8f7d09bc8caf0a9452db
Piotr Celigowski <piotrcel2@gmail.com>
2016-12-29 01:16:43 +0100

    bomb lottery

--- js/app.js
+++ js/app.js
@@ -1,11 +1,82 @@
+/* wartości areny:
+    
+    99 - bomba
+    0 - 0 bomb w pobliżu
+    1 - 1 bomba w pobliżu
+    itd
+    
+    wartości arenymark:
+    
+    0 - nieklikniete
+    1 - klikniete (bez bomby)
+    2 - postawiona flaga
+    3 - klikniete z bomba? */
+
+
 document.addEventListener("DOMContentLoaded", function(){
     
+    var rows = document.getElementsByClassName("row");
+    var fields = document.getElementsByClassName("field");
     
-    console.log('elo');
+    for (var i=0; i<fields.length; i++){
+        
+        fields[i].classList.add('unclicked');
+    }
     
-    var example = document.getElementById('example');
-    var context = example.getContext('2d');
-    context.fillStyle = "rgb(255,0,0)";
-    context.fillRect(10, 30, 50, 50);
+    
+    var bombsleft = 0;
+    var arena = [];
+    var arenamark = [];
+    
+    var bombisplaced = false; //zmienna pomocnicza do losowania miejsc bomb
+    
+    
+    
+    for (var i=0; i<= 23; i++){ //rysowanie tablic
+        arena[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        arenamark[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+    
+    
+    
+    
+    
+    for (var i=1; i<=112; i++){ //rozlosowanie 112 bomb
+        
+        while (bombisplaced == false) {
+            
+            var y = Math.round(Math.random() * 23);
+            var x = Math.round(Math.random() * 23);
+            
+            if (arena[y][x] == 0){
+                
+                arena[y][x] = 99;
+                bombisplaced = true;
+            }
+        
+        }
+        
+        bombisplaced = false;
+        
+    }
+    
+    /* wywala wszystkie bomby na mapie
+    for (var i=0; i<=23; i++){
+        for (var j=0; j<=23; j++){
+        
+            if (arena[i][j] == 99){
+                rows[i].children[j].classList.add('bombhere');
+            }
+            
+        }
+    }
+    */
+    
+    
+    console.log(arena);
+    
+    
+    
+    
     
 });
